1150031bdb0f0c5e128694ebc1c7ac8bf0e9003d
Nighthawk <jb@Nighthawks-MBP.attlocal.net>
2020-11-12 11:19:11 -0600

    Compile Success; Style not loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+//import { AuthGuard } from './guards/auth.guard';
+// import { AngularFireModule } from 'angularfire2';
+import { environment } from '../environments/environment';
+// import { AngularFireAuthModule } from 'angularfire2/auth';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './components/register/register.component';
@@ -8,6 +14,31 @@ import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { DaysCalculatorComponent } from './components/days-calculator/days-calculator.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+// Font awesome imports
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faMugHot, faHeart,
+    faTh, faThList,
+    faSignInAlt, faUserPlus,
+    faPlaneDeparture, faMapMarked,
+    faSearch, faWindowClose, faPen,
+    faHiking, faCalendarAlt, faArchway, faComments,
+    faCogs, faEdit, faTrashAlt, faArrowCircleLeft, faSave,
+    faUpload
+} from '@fortawesome/free-solid-svg-icons';
+
+library.add(faHeart, faMugHot,
+    faTh, faThList,
+    faSignInAlt, faUserPlus,
+    faPlaneDeparture, faMapMarked,
+    faSearch, faWindowClose, faPen,
+    faHiking, faCalendarAlt, faArchway, faComments,
+    faCogs, faEdit, faTrashAlt, faArrowCircleLeft, faSave,
+    faUpload
+);
+
 
 @NgModule({
   declarations: [
@@ -20,9 +51,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    MatDatepickerModule],
+    // CookieService,
+    // {provide: HTTP_INTERCEPTORS, useClass: AuthenticationInterceptor, multi: true}],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
