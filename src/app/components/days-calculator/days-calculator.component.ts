7fbf08f7da7e0f8c30b814e45a19a8d983cc8e2d
Nighthawk <jb@Nighthawks-MBP.attlocal.net>
2020-11-12 15:54:29 -0600

    Days remaining correct; Next Fill Date - Day field not moving

--- src/app/components/days-calculator/days-calculator.component.ts
+++ src/app/components/days-calculator/days-calculator.component.ts
@@ -9,25 +9,34 @@ export class DaysCalculatorComponent implements OnInit {
 
   // TEST DATA
   startDate = new Date();
-
   issueDate : Date = new Date();
+ 
   // expirationDate : Date = new Date();
   FILL_DAYS = 45;
   nextFill: Date = new Date();
+  fillDateStr : String;
+  daysRemaining: number;
 
   constructor() { 
+   
   }
 
   ngOnInit(): void {
-  this.startDate.setFullYear(2020);
-  this.startDate.setMonth(5);
-  this.startDate.setDate(2);
+  let today = new Date();
+  this.startDate.setFullYear(2021);
+  this.startDate.setMonth(2);
+  this.startDate.setDate(7);
+// Need to catch December dates moving into new year
 
   this.nextFill.setMonth(this.startDate.getMonth() + 1);
   this.nextFill.setFullYear(this.startDate.getFullYear() + 45);
   this.nextFill.setFullYear(2020);
+  this.fillDateStr = this.nextFill.toLocaleString();
 
-  console.log(this.nextFill.toDateString());
+
+  this.daysRemaining = Math.abs(Math.floor((this.nextFill.getTime() - today.getTime())/(1000*60*60*24)));
+
+  console.log(this.daysRemaining);
 
   }
 
